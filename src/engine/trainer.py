8b09437a7dc008e24c5130d9cfd129599fcfa282
“wooqi00” <whddnr0905@gmail.com>
2025-12-30 14:09:56 +0900

    boundary_loss weight 수정

--- src/engine/trainer.py
+++ src/engine/trainer.py
@@ -131,7 +131,7 @@ def train(model, data_loader, val_loader, criterion, optimizer, cfg):
                 loss_boundary = criterion(pred_boundary, boundary_gt)
 
                 # 4️⃣ Total loss
-                loss = loss_seg + 0.5 * loss_boundary.detach()
+                loss = loss_seg + 0.05 * loss_boundary.detach()
 
             else:
                 # Baseline (U-Net++)
