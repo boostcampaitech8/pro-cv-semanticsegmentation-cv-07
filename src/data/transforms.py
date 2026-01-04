5a333be5ed4cf0c2a7d57638847a8342d54b8769
JeongBin Choi <20210900@sungshin.ac.kr>
2026-01-04 05:18:36 +0000

    feat: add augmentation

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -1,16 +1,47 @@
 import albumentations as A
+import cv2
 
 def get_train_transform():
-    return A.Compose([
-        A.Resize(512, 512),
-    ])
+    t = [
+        A.Resize(2048, 2048),
+        A.Affine(
+            scale=(1 - 0.05, 1 + 0.05),
+            translate_percent=None,
+            rotate=0,
+            shear=0,
+            interpolation=cv2.INTER_LINEAR,
+            mode=cv2.BORDER_CONSTANT,
+            cval=0,
+            cval_mask=0,
+            p=0.8,
+        ),
+        A.Affine(
+            rotate=(-30, 30),
+            translate_percent={"x": 0.0, "y": 0.15}, 
+            scale=1.0,
+            shear=0,
+            interpolation=cv2.INTER_LINEAR,
+            mode=cv2.BORDER_CONSTANT,
+            cval=0,
+            cval_mask=0,
+            p=0.8,
+        ),
+        A.HorizontalFlip(p=0.5),
+        A.RandomBrightnessContrast(
+            brightness_limit=0.0,
+            contrast_limit=(0.1, 0.3),
+            p=0.5
+        ),
+    ]
+
+    return A.Compose(t)
 
 def get_valid_transform():
     return A.Compose([
-        A.Resize(512, 512)
+        A.Resize(2048, 2048)
     ])
 
 def get_test_transform():
     return A.Compose([
-        A.Resize(512, 512)
+        A.Resize(2048, 2048)
     ])
