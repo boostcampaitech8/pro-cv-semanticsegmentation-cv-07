47141ee52eb135551df0689170b6333f06429908
JeongBin Choi <20210900@sungshin.ac.kr>
2026-01-08 06:42:41 +0000

    refactor: change if to elif

--- src/models/smp_model.py
+++ src/models/smp_model.py
@@ -25,7 +25,7 @@ def build_smp_model(cfg, encoder_weights="imagenet", in_channels=3, classes=29):
         from src.models.swin_unet import build_swin_unet
         return build_swin_unet(cfg)
     
-    if model_name not in SMP_MODELS:
+    elif model_name not in SMP_MODELS:
         raise ValueError(f"Unknown model_name {cfg.model_name}, choose from {list(SMP_MODELS.keys())}")
 
     model_cls = SMP_MODELS[model_name]
